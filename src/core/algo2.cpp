723354956136bef4f6a9c90b2e7f3c137fb81e1e
Damien <damien@ubuntu.ubuntu-domain>
2014-02-10 10:47:25 +0100

    commit

--- src/core/algo2.cpp
+++ src/core/algo2.cpp
@@ -6,34 +6,96 @@
 namespace vcity
 {
 ////////////////////////////////////////////////////////////////////////////////
-void Algo2::fixBuilding(const std::vector<URI>& uris)
-{
-    if(uris.size() >= 2)
+    void Algo2::processRec(citygml::CityObject* node)
     {
-        citygml::CityObject* building = nullptr;
-        citygml::CityObject* terrain = nullptr;
+        log() << "processRec \n";
 
-        building = app().getScene().getNode(uris[0]);
-        terrain = app().getScene().getNode(uris[1]);
-
-        if(building->getType() != citygml::COT_Building)
+        if(node->getType() == citygml::COT_WallSurface)
         {
-            citygml::CityObject* tmp = building;
-            building = terrain;
-            terrain = tmp;
+            log() << "MUR \n";
         }
 
-        log() << "building : " << building->getId() << "\n";
-        log() << "terrain : " << terrain->getId() << "\n";
+        // parse geometry
+        std::vector<citygml::Geometry*>& geoms = node->getGeometries();
+        std::vector<citygml::Geometry*>::iterator itGeom = geoms.begin();
+        for(; itGeom != geoms.end(); ++itGeom)
+        {
+            // parse polygons
+            std::vector<citygml::Polygon*>& polys = (*itGeom)->getPolygons();
+            std::vector<citygml::Polygon*>::iterator itPoly = polys.begin();
+            for(; itPoly != polys.end(); ++itPoly)
+            {
+                // get linear ring
+                citygml::LinearRing* ring = (*itPoly)->getExteriorRing();
+                std::vector<TVec3d>& vertices = ring->getVertices();
+                std::vector<TVec3d>::iterator itVertices = vertices.begin();
+                for(; itVertices != vertices.end(); ++itVertices)
+                {
+                    // Points
+                    TVec3d point = *itVertices;
+                    log() << "X:" << point.x << "\n";
+                    log() << "Y:" << point.y << "\n";
+                    log() << "Z:" << point.z << "\n";
+                }
+            }
+        }
 
-        // parcourir les murs
-        // trouver les ponits du bas
-
-        // tester les intersections
-
-        // bouger les points si il faut
+        citygml::CityObjects& cityObjects = node->getChildren();
+        citygml::CityObjects::iterator itObj = cityObjects.begin();
+        for(; itObj != cityObjects.end(); ++itObj)
+        {
+            processRec(*itObj); //Appel récursif
+        }
     }
-}
+
+////////////////////////////////////////////////////////////////////////////////
+    void Algo2::fixBuilding(const std::vector<URI>& uris)
+    {
+
+        log() << "fixBuilding \n";
+
+
+        const std::vector<vcity::URI>& uris2 = vcity::app().getSelectedNodes();
+
+        vcity::URI uri = uris[0];
+        citygml::CityObject* obj = app().getScene().getNode(uri);
+
+        log() << "test : " << obj->getId() << "\n";
+
+
+        if(uris.size() >= 2)
+        {
+            citygml::CityObject* building = nullptr;
+            citygml::CityObject* terrain = nullptr;
+
+            building = app().getScene().getNode(uris[0]);
+            terrain = app().getScene().getNode(uris[1]);
+
+            if(building->getType() != citygml::COT_Building)
+            {
+                citygml::CityObject* tmp = building;
+                building = terrain;
+                terrain = tmp;
+            }
+
+            log() << "building : " << building->getId() << "\n";
+            log() << "terrain : " << terrain->getId() << "\n";
+
+            /***************************** Parcourt les murs ******************************/
+            processRec(building);
+
+            /************************** Trouve les points du bas **************************/
+
+
+            /*************************** Teste les intersections **************************/
+            processRec(terrain);
+
+
+            /************************* Bouge les points si besoin *************************/
+
+
+        }
+    }
 ////////////////////////////////////////////////////////////////////////////////
 } // namespace vcity
 ////////////////////////////////////////////////////////////////////////////////
--- src/core/algo2.hpp
+++ src/core/algo2.hpp
@@ -2,19 +2,23 @@
 #define __ALGO2_HPP__
 ////////////////////////////////////////////////////////////////////////////////
 #include "URI.hpp"
+#include "scene.hpp"
+#include "dataprofile.hpp"
+#include "settings.hpp"
+#include "tools/log.hpp"
 ////////////////////////////////////////////////////////////////////////////////
 namespace vcity
 {
-class Algo2
-{
-public:
-    void fixBuilding(const std::vector<URI>& uris);
+    class Algo2
+    {
+        public:
+            void fixBuilding(const std::vector<URI>& uris);
+            void processRec(citygml::CityObject *);
 
-private:
+        private:
 
-};
+    };
 ////////////////////////////////////////////////////////////////////////////////
 } // namespace vcity
 ////////////////////////////////////////////////////////////////////////////////
 #endif // __ALGO2_HPP__
-
