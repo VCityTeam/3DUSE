82ccc07a86f7da8950028d4265a60de200787be3
Martial Tola <martial.tola@liris.cnrs.fr>
2016-01-04 16:42:34 +0100

    v0.3.2 -> 31/12/2016

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
-    std::cout << "3D-Use version 0.2.5" << std::endl;
+    std::cout << "3D-Use version 0.3.2" << std::endl;
 
 #ifdef Q_WS_X11
     QCoreApplication::setAttribute(Qt::AA_X11InitThreads);
