877441df56392ef604abd9b95ee3a25bcefad705
Martial Tola <martial.tola@liris.cnrs.fr>
2016-01-25 17:37:46 +0100

    update travis & cmake/FindASSIMP.cmake

--- cmake/FindASSIMP.cmake
+++ cmake/FindASSIMP.cmake
@@ -36,7 +36,7 @@ ELSE (WIN32)
 		/sw/lib
 		/opt/local/lib
 		${ASSIMP_ROOT_DIR}/lib
-		${ASSIMP_ROOT_DIR}/build
+		${ASSIMP_ROOT_DIR}/build/code # just a hack for Travis CI
 		DOC "The ASSIMP library")
 ENDIF (WIN32)
 
