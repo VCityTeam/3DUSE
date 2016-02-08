5459c9150092f3e3c694e733a56ab3c8adf8eddc
Jérémy Edert <jeremy.edert@insa-lyon.fr>
2016-02-08 11:11:52 +0100

    fixed some bugs in dialogs for creationDate & terminationDate

--- src/gui/dialogYearOfConst.cpp
+++ src/gui/dialogYearOfConst.cpp
@@ -4,10 +4,10 @@
 #include "osg/ValueObject"
 
 DialogYearOfConst::DialogYearOfConst(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::DialogYearOfConst)
+	QDialog(parent),
+	ui(new Ui::DialogYearOfConst)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
 	ui->comboBox->insertItem(0,"",QVariant(0));
 	ui->comboBox->insertItem(1,"yearOfConstruction",QVariant(1));
 	ui->comboBox->insertItem(2,"creationDate",QVariant(2));
@@ -17,7 +17,7 @@ DialogYearOfConst::DialogYearOfConst(QWidget *parent) :
 
 DialogYearOfConst::~DialogYearOfConst()
 {
-    delete ui;
+	delete ui;
 }
 
 void DialogYearOfConst::editDates(const vcity::URI& uri)
@@ -33,10 +33,13 @@ void DialogYearOfConst::editDates(const vcity::URI& uri)
 }
 
 void DialogYearOfConst::editTileDates(const vcity::URI& uri)
+	// Called if object is a Tile
 {
 
 	ui->comboBox->setCurrentIndex(0);
 	ui->dateConstrEdit->setEnabled(false);
+	yearOfConstruction = 2000;
+	creationDate = "2000-01-01T00:00:00";
 
 	//window execution
 	int res = exec();
@@ -47,6 +50,7 @@ void DialogYearOfConst::editTileDates(const vcity::URI& uri)
 		osg::ref_ptr<osg::Group> grp = appGui().getOsgScene()->getNode(uri)->asGroup();
 		QDateTime creaDate = ui->dateConstrEdit->dateTime();
 		int index = ui->comboBox->currentIndex();
+		// Edit OSG nodes (Tile children)
 		if(grp)
 		{
 			for(unsigned int i=0; i<grp->getNumChildren(); ++i)
@@ -54,16 +58,11 @@ void DialogYearOfConst::editTileDates(const vcity::URI& uri)
 				osg::ref_ptr<osg::Node> child = grp->getChild(i);
 				switch (index)
 				{
-				case 1:
-					//o->setAttribute("yearOfConstruction",creaDate.toString("yyyy").toStdString());
+				case 1: //user added/edited yearOfConstruction
 					child->setUserValue("yearOfConstruction",creaDate.date().year());
 					break;
-				case 2:
-					{
-						//o->setAttribute("creationDate",creaDate.toString(Qt::ISODate).toStdString());
-						QString cDate = (creaDate.toString(Qt::ISODate));
-						child->setUserValue("creationDate",cDate.toStdString());
-					}
+				case 2: //user added/edited creationDate
+					child->setUserValue("creationDate",creaDate.toString(Qt::ISODate).toStdString());
 					break;
 				default:
 					//do nothing
@@ -71,8 +70,9 @@ void DialogYearOfConst::editTileDates(const vcity::URI& uri)
 				}
 			}
 		}
+		// Edit CityGML Objects
 		for(citygml::CityObject* o : tile->getCityModel()->getCityObjectsRoots())
-        {
+		{
 			switch (index)
 			{
 			case 1:
@@ -86,20 +86,21 @@ void DialogYearOfConst::editTileDates(const vcity::URI& uri)
 				//do nothing
 				break;
 			}               
-        }
+		}
 
 	}
 }
 
 void DialogYearOfConst::editObjectDates(const vcity::URI& uri)
+	// Called if object is a not a Tile
 {
 	citygml::CityObject* obj = nullptr;
 	uri.resetCursor();
 
-    obj = vcity::app().getScene().getCityObjectNode(uri);
+	obj = vcity::app().getScene().getCityObjectNode(uri);
 	osg::ref_ptr<osg::Node> node = appGui().getOsgScene()->getNode(uri);
 
-    bool a = node->getUserValue("yearOfConstruction", yearOfConstruction);
+	bool a = node->getUserValue("yearOfConstruction", yearOfConstruction);
 	bool b = node->getUserValue("creationDate", creationDate);
 
 	if (a)
@@ -107,39 +108,40 @@ void DialogYearOfConst::editObjectDates(const vcity::URI& uri)
 		ui->comboBox->setCurrentIndex(1);
 		ui->dateConstrEdit->setEnabled(true);
 		ui->dateConstrEdit->setDateTime(QDateTime(QDate(yearOfConstruction,1,1)));
+		creationDate = std::to_string(yearOfConstruction)+"-01-01T00:00:00";
 	} 
 	else if (b)
 	{
 		ui->comboBox->setCurrentIndex(2);
 		ui->dateConstrEdit->setEnabled(true);
 		std::string str_cDate = creationDate;
-		QDate cDate = QDate::fromString(QString::fromStdString(str_cDate),Qt::ISODate);
-		ui->dateConstrEdit->setDate(cDate);
+		QDateTime cDate = QDateTime::fromString(QString::fromStdString(str_cDate),Qt::ISODate);
+		ui->dateConstrEdit->setDateTime(cDate);
+		yearOfConstruction = cDate.date().year();
 	}
 	else
 	{
 		ui->comboBox->setCurrentIndex(0);
 		ui->dateConstrEdit->setEnabled(false);
+		yearOfConstruction = 2000;
+		creationDate = "2000-01-01T00:00:00";
 	}
 
 	//window execution
 	int res = exec();
 	if (res)
 	{
-		QDate creaDate = ui->dateConstrEdit->date();
+		QDateTime creaDate = ui->dateConstrEdit->dateTime();
 		int index = ui->comboBox->currentIndex();
 		switch (index)
 		{
-		case 1:
+		case 1: //user added/edited yearOfConstruction
 			obj->setAttribute("yearOfConstruction",creaDate.toString("yyyy").toStdString());
-			node->setUserValue("yearOfConstruction",creaDate.year());
+			node->setUserValue("yearOfConstruction",creaDate.date().year());
 			break;
-		case 2:
-			{
-				obj->setAttribute("creationDate",creaDate.toString(Qt::ISODate).toStdString());
-				QString cDate = (creaDate.toString(Qt::ISODate));
-				node->setUserValue("creationDate",cDate.toStdString());
-			}
+		case 2: //user added/edited creationDate
+			obj->setAttribute("creationDate",creaDate.toString(Qt::ISODate).toStdString());
+			node->setUserValue("creationDate",creaDate.toString(Qt::ISODate).toStdString());
 			break;
 		default:
 			//do nothing
