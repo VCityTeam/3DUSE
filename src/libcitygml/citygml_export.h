04e37f8b0c996ac7abe902a22a92bf1c18c3c933
frogsapo <eric.boix@gmail.com>
2016-05-19 22:37:59 +0200

    Still trying to almost revert...:

--- src/libcitygml/citygml_export.h
+++ src/libcitygml/citygml_export.h
@@ -1,2 +1,41 @@
-// FAILURE
-#define CITYGML_EXPORT
+
+#ifndef CITYGML_EXPORT_H
+#define CITYGML_EXPORT_H
+
+#ifdef CITYGML_STATIC_DEFINE
+#  define CITYGML_EXPORT
+#  define CITYGML_NO_EXPORT
+#else
+#  ifndef CITYGML_EXPORT
+#    ifdef citygml_EXPORTS
+        /* We are building this library */
+#      define CITYGML_EXPORT __attribute__((visibility("default")))
+#    else
+        /* We are using this library */
+#      define CITYGML_EXPORT __attribute__((visibility("default")))
+#    endif
+#  endif
+
+#  ifndef CITYGML_NO_EXPORT
+#    define CITYGML_NO_EXPORT __attribute__((visibility("hidden")))
+#  endif
+#endif
+
+#ifndef CITYGML_DEPRECATED
+#  define CITYGML_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef CITYGML_DEPRECATED_EXPORT
+#  define CITYGML_DEPRECATED_EXPORT CITYGML_EXPORT CITYGML_DEPRECATED
+#endif
+
+#ifndef CITYGML_DEPRECATED_NO_EXPORT
+#  define CITYGML_DEPRECATED_NO_EXPORT CITYGML_NO_EXPORT CITYGML_DEPRECATED
+#endif
+
+#define DEFINE_NO_DEPRECATED 0
+#if DEFINE_NO_DEPRECATED
+# define CITYGML_NO_DEPRECATED
+#endif
+
+#endif
--- src/libcitygml/utils/citygmlutils_export.h
+++ src/libcitygml/utils/citygmlutils_export.h
@@ -1,2 +1,41 @@
-// FAILURE
-#define CITYGMLUTILS_EXPORT
+
+#ifndef CITYGMLUTILS_EXPORT_H
+#define CITYGMLUTILS_EXPORT_H
+
+#ifdef CITYGMLUTILS_STATIC_DEFINE
+#  define CITYGMLUTILS_EXPORT
+#  define CITYGMLUTILS_NO_EXPORT
+#else
+#  ifndef CITYGMLUTILS_EXPORT
+#    ifdef citygmlutils_EXPORTS
+        /* We are building this library */
+#      define CITYGMLUTILS_EXPORT __attribute__((visibility("default")))
+#    else
+        /* We are using this library */
+#      define CITYGMLUTILS_EXPORT __attribute__((visibility("default")))
+#    endif
+#  endif
+
+#  ifndef CITYGMLUTILS_NO_EXPORT
+#    define CITYGMLUTILS_NO_EXPORT __attribute__((visibility("hidden")))
+#  endif
+#endif
+
+#ifndef CITYGMLUTILS_DEPRECATED
+#  define CITYGMLUTILS_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef CITYGMLUTILS_DEPRECATED_EXPORT
+#  define CITYGMLUTILS_DEPRECATED_EXPORT CITYGMLUTILS_EXPORT CITYGMLUTILS_DEPRECATED
+#endif
+
+#ifndef CITYGMLUTILS_DEPRECATED_NO_EXPORT
+#  define CITYGMLUTILS_DEPRECATED_NO_EXPORT CITYGMLUTILS_NO_EXPORT CITYGMLUTILS_DEPRECATED
+#endif
+
+#define DEFINE_NO_DEPRECATED 0
+#if DEFINE_NO_DEPRECATED
+# define CITYGMLUTILS_NO_DEPRECATED
+#endif
+
+#endif
