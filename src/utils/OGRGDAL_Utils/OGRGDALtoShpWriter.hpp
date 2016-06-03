7d64ad84a2d824f580dfb587f549e0ea8244b40f
frogsapo <eric.boix@gmail.com>
2016-06-03 22:55:08 +0200

    Revert "Inhibiting more VC++ gdal related warnings."
    This reverts commit 3b750687b0279511505e23b84d75caa3bb1d4151.

--- src/utils/OGRGDAL_Utils/OGRGDALtoShpWriter.hpp
+++ src/utils/OGRGDAL_Utils/OGRGDALtoShpWriter.hpp
@@ -2,15 +2,11 @@
 ////////////////////////////////////////////////////////////////////////////////
 #ifndef __OGRGDALTOSHPWRITER_HPP__
 #define __OGRGDALTOSHPWRITER_HPP__
-
-#include <string>
-#ifdef _MSC_VER                // Inhibit dll-interface warnings concerning
-# pragma warning(disable:4251) // gdal-1.11.4 internals (cpl_string.h) when
-#endif                         // including ogrsf_frmts.h on VCC++
-#include <ogrsf_frmts.h>
-
+////////////////////////////////////////////////////////////////////////////////
+#include "ogrsf_frmts.h"
+#include <stdlib.h>
+////////////////////////////////////////////////////////////////////////////////
 void SaveGeometrytoShape(std::string name, const OGRGeometryCollection* G);
-
 void SaveGeometrytoShape(std::string name, const OGRGeometry* G);
-
+////////////////////////////////////////////////////////////////////////////////
 #endif // __OGRGDALTOSHPWRITER_HPP__
