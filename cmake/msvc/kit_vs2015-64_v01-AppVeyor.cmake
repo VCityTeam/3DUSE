1d951347ee6b47182379a86efe72935ea9586556
frogsapo <eric.boix@gmail.com>
2016-11-08 23:27:55 +0100

    Cmake: final cleanup of APPVEYOR cmake variable.

--- cmake/msvc/kit_vs2015-64_v01-AppVeyor.cmake
+++
@@ -1,6 +0,0 @@
-# MSVC - KIT VS 2015 x64 - V01
-
-#### The following variables are directly set in appveyor.yml script
-# * QT5_DIR
-# * BOOST_ROOT  (PCL sub-dependency)
-# * FLANN_ROOT  (PCL sub-dependency)
--- cmake/msvc/kit_vs2015-64_v01-desktop.cmake
+++
@@ -1,9 +0,0 @@
-# MSVC - KIT VS 2015 x64 - V01
-
-### FIXME remove this file
-### if( BUILD_GUI_QT5 )
-###   set(QT5_DIR           ${VCITY_KIT_ROOT}/Qt/Qt5.6.0/5.6/msvc2015_64)
-### endif()
-
-###### PCL and its sub-dependencies
-### set(BOOST_ROOT          ${VCITY_KIT_ROOT}/boost_1_59_0)
