3a5b1f3bf1f3b868a18210fed9633666fe159846
EricBoix <eric.boix@gmail.com>
2017-07-18 11:08:20 +0200

    Fix of erroneous previous commit.

--- src/gui/moc/plugindialog.hpp
+++ src/gui/moc/plugindialog.hpp
@@ -3,6 +3,7 @@
 // (Refer to accompanying file LICENSE.md or copy at
 //  https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html )
 
+#ifndef PLUGINDIALOG_H
 #define PLUGINDIALOG_H
 
 #include <QDialog>
